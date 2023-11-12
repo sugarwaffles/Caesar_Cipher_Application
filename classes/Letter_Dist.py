25dbe8bb7c9ac635f8870cad62c8b40d077c2e17
wilfred <2a05.2237503.wilfreddjumin@gmail.com>
2023-11-12 23:15:27 +0800

    Managed to use the SortedList for TOP 5 FREQ, added functions in class

--- classes/Letter_Dist.py
+++ classes/Letter_Dist.py
@@ -58,79 +58,97 @@ class LetterFrequencyDistribution:
             frequencies = {k: round(v / total_letters * 100, 2) for k, v in letter_counts.items()}
 
             # Print top 5 frequencies
-            top5_freq_table = []
-            # Setting the "Headers" for TOP 5 FREQ
-            top5_freq_table.append("TOP 5 FREQ")
-            top5_freq_table.append("-" * 11)
-
-            sorted_list = SortedList()
-            for letter, frequency in sorted(frequencies.items(), key=lambda x: (-x[1], x[0])):
-                sorted_list.insert(SortedList.Node((letter, frequency)))
-
-            # Iterate over the first five nodes in the sorted list
-            current_node = sorted_list.headNode
-            for _ in range(5):
-                if current_node is None:
-                    break
-                letter, frequency = current_node.data
-                if frequency > 10:
-                    top5_freq_table.append(f"| {letter.upper()} -{frequency}%")
-                else:
-                    top5_freq_table.append(f"| {letter.upper()} - {frequency}%")
-                current_node = current_node.nextNode
+            top5_freq_table = self.calculate_top5_frequencies(frequencies)
 
             # Total width represents the num of characters(space in this case) to fill the missing spaces (to the left of the str) which will be used for the y-axis creation
             total_width = 80
 
             # Create a list to store the histogram
-            histogram = []
+            histogram = self.create_histogram(letter_counts)
 
-            # Populate the histogram with '*' for each letter
-            for y_index in range(25, -1, -1):  # Start from Z and go backward
-                line = ''
-                for letter in string.ascii_uppercase:  # Use ascii_uppercase for X-axis labels
-                    count = letter_counts.get(letter.lower(), 0)
-                    # Only print a ' * ' when
-                    if count > y_index:
-                        line += ' * '
-                    else:
-                        line += '   '
-
-                histogram.append(line)
+            # Print the histogram
+            self.print_histogram(histogram, frequencies, total_width, top5_freq_table)
             
-            line_counter = 0
+            #Print the X-axis
+            self.X_axis(total_width)
 
-            for line_hist, letter in zip(histogram, string.ascii_uppercase):
-                percentage = frequencies.get(letter.lower(), 0)
+    def calculate_top5_frequencies(self, frequencies):
+        top5_freq_table = []
+        top5_freq_table.append("TOP 5 FREQ")
+        top5_freq_table.append("-" * 11)
 
-                # Find starting level for TOP 5 FREQ table
-                if 'K' <= letter <= 'Q':
-                    # Only print if there are lines left in top5_freq_table
-                    if line_counter < len(top5_freq_table):
-                        line = "{} | {}- {:.2f}% {:>15s}".format(line_hist, letter, percentage, top5_freq_table[line_counter])
-                        line = " " + line.rjust(total_width)
-                        print(line)
-                        line_counter += 1
+        sorted_list = SortedList()
+        for letter, frequency in sorted(frequencies.items(), key=lambda x: (-x[1], x[0])):
+            sorted_list.insert(SortedList.Node((letter, frequency)))
+
+        # Iterate over the first five nodes in the sorted list
+        current_node = sorted_list.headNode
+        for _ in range(5):
+            if current_node is None:
+                break
+            letter, frequency = current_node.data
+            if frequency > 10:
+                top5_freq_table.append(f"| {letter.upper()} -{frequency}%")
+            else:
+                top5_freq_table.append(f"| {letter.upper()} - {frequency}%")
+            current_node = current_node.nextNode
+
+        return top5_freq_table
+
+    def create_histogram(self, letter_counts):
+        histogram = []
+
+        # Populate the histogram with '*' for each letter
+        for y_index in range(25, -1, -1):  # Start from Z and go backward
+            line = ''
+            for letter in string.ascii_uppercase:  # Use ascii_uppercase for X-axis labels
+                count = letter_counts.get(letter.lower(), 0)
+                # Only print a ' * ' when
+                if count > y_index:
+                    line += ' * '
                 else:
-                    # Formatting double-digit percentages
-                    if percentage > 10:
-                        line = "{} | {}-{:.2f}%".format(line_hist, letter, percentage)
-                    else:
-                        line = "{} | {}- {:.2f}%".format(line_hist, letter, percentage)
+                    line += '   '
 
-                    # Reset spaces before line after finishing printing TOP 5 FREQ table to the right of the y-axis
+            histogram.append(line)
 
-                    output_line = " " + line.rjust(total_width)
-                    print(output_line)
+        return histogram
 
-            # Print "X axis" using the correct unicode U+2500 (box drawing single horizontal line)
-            print('\u2500' * (total_width))
+    def print_histogram(self, histogram, frequencies, total_width, top5_freq_table):
+        line_counter = 0
 
-            # Print bottom letters
-            print(' ', end=' ')
-            for letter in string.ascii_uppercase:
-                print(f"{letter}  ", end='')
-            print()
+        for line_hist, letter in zip(histogram, string.ascii_uppercase):
+            percentage = frequencies.get(letter.lower(), 0)
+
+            # Find starting level for TOP 5 FREQ table
+            if 'K' <= letter <= 'Q':
+                # Only print if there are lines left in top5_freq_table
+                if line_counter < len(top5_freq_table):
+                    line = "{} | {}- {:.2f}% {:>15s}".format(line_hist, letter, percentage, top5_freq_table[line_counter])
+                    line = " " + line.rjust(total_width)
+                    print(line)
+                    line_counter += 1
+            else:
+                # Formatting double-digit percentages
+                if percentage > 10:
+                    line = "{} | {}-{:.2f}%".format(line_hist, letter, percentage)
+                else:
+                    line = "{} | {}- {:.2f}%".format(line_hist, letter, percentage)
+
+                # Reset spaces before line after finishing printing TOP 5 FREQ table to the right of the y-axis
+
+                output_line = " " + line.rjust(total_width)
+                print(output_line)
+                
+    def X_axis(self,total_width):
+        
+        # Print "X axis" using the correct unicode U+2500 (box drawing single horizontal line)
+        print('\u2500' * (total_width))
+
+        # Print bottom letters
+        print(' ', end=' ')
+        for letter in string.ascii_uppercase:
+            print(f"{letter}  ", end='')
+        print()
 
 
 
